ee2817a9fc6a20165f17e3827b31e71188019933
Juste KOCOU <justekocou21@gmail.com>
2025-08-11 18:39:03 +0100

    Add files via upload

---
+++ Armoire_de_laverie/Armoire_de_laverie.ino
@@ -0,0 +1,42 @@
+#include <Keypad.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// Définition du clavier
+const byte ROWS = 4; // 4 lignsses
+const byte COLS = 3; // 3 colonnes
+
+LiquidCrystal_I2C lcd(lcdAddress, lcdColumns, lcdRows);
+
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+
+byte rowPins[ROWS] = {16, 5, 4, 0};     // Connecté aux broches du clavier (lignes)
+byte colPins[COLS] = {2, 14, 12};        // Connecté aux broches du clavier (colonnes)
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+
+void setup() {
+  // put your setup code here, to run once:
+  //---------------------------------LCD INIT --------------------------------------------------//
+lcd.init(); 
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Init systeme... ");
+
+
+  
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
