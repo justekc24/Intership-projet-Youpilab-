79da8b89a06459137f1e601b0dc5772b0cded7b2
Juste KOCOU <justekocou21@gmail.com>
2025-08-12 17:33:21 +0100

    new Armoire_de_laverie.ino

--- Armoire_de_laverie/Armoire_de_laverie.ino
+++ Armoire_de_laverie/Armoire_de_laverie.ino
@@ -1,7 +1,9 @@
 #include <Keypad.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-
+//Pour le PF8475
+#define PCF8574_ADDR 0x20 
+byte pcfState = 0xFF; // Toutes les broches en HIGH au démarrage (entrée ou sortie HIGH)
 // Définition du clavier
 const byte ROWS = 4; // 4 lignsses
 const byte COLS = 3; // 3 colonnes
@@ -23,6 +25,7 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 void setup() {
   // put your setup code here, to run once:
+  Wire.begin(); // Pour le PF8475
   //---------------------------------LCD INIT --------------------------------------------------//
 lcd.init(); 
   lcd.backlight();
@@ -31,15 +34,13 @@ lcd.init();
   lcd.print("Init systeme... ");
 
 
-  
-
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
 void openDoor()
 {
    for (int i = 0; i <= 90; i++) {
@@ -65,5 +66,23 @@ void legIn()
 }
   
   
+void motorActive()
+{
+  // Mettre P2 à HIGH
+  bitSet(pcfState, 2); // P2 = 1
+  Wire.beginTransmission(PCF8574_ADDR);
+  Wire.write(pcfState);
+  Wire.endTransmission();
+  delay(1000);
+
+  // Mettre P2 à LOW
+  bitClear(pcfState, 2); // P2 = 0
+  Wire.beginTransmission(PCF8574_ADDR);
+  Wire.write(pcfState);
+  Wire.endTransmission();
+  delay(1000);
+}
+
+
 
 
